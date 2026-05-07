597c1d789a15e7a4c5b9353252fd544da183589b
cevapizza11 <cevapizza11@gmail.com>
2026-05-07 15:45:01 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -3,3 +3,30 @@ console.log("JS OK");
 function login() {
   alert("LOGIN OK");
 }
+const firebaseConfig = {
+  apiKey: "AIzaSyDhqKmCJJx5ScXUxaoip1eMiy10P0BvD9U",
+  authDomain: "marmite-bleue.firebaseapp.com",
+  projectId: "marmite-bleue",
+  storageBucket: "marmite-bleue.firebasestorage.app",
+  messagingSenderId: "938472624829",
+  appId: "1:938472624829:web:edd7453c2589c820dfddd4"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+function login() {
+
+  const userEmail = document.getElementById("email").value;
+  const userPassword = document.getElementById("password").value;
+
+  auth.signInWithEmailAndPassword(userEmail, userPassword)
+    .then(() => {
+      alert("Connexion OK");
+    })
+    .catch((error) => {
+      alert(error.message);
+      console.log(error);
+    });
+}
